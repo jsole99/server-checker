59cecb28e75495cd5f67b6c3e11d56b6beceaa2b
Jeff <jeffery.sole@outlook.com>
2026-04-15 01:46:41 +0000

    Adding better logic control for menu - working on while loop to validate menu input for only numbers

--- main.py
+++ main.py
@@ -1,28 +1,81 @@
 import re
+import os
 
-print("JDS Service Checker 1.0 Running...")
-print("Please select an option:")
-print("1. Check Server Status")
-print("2. Add New Server")
-print("3. Remove Server")
-print("4. Exit")
-menuSelection = input("Selection: ")
+## need to get menu function working
+def menu():
+    menu_list = ["Check Server Status", "Add New Server", "Remove Server", "Exit"]
 
-if menuSelection == "1":
-    serverIP = input("Enter Server IP or DNS: ")
-    print("Checking status of " + serverIP + "...")
-    #check if the input follows an ip strucutre
-    x = re.search(r"([\d]+\.[\d]+\.[\d]+\.[\d+]+)", serverIP)
-    print(x)
-    #check if the input follows a dns structure
-    #if the input is neither an IP nor DNS - throw error
-    #else we will start to check the status of the server
+    # iterates menu
+    for i in menu_list:
+        print(str(menu_list.index(i) + 1) + ". " + i)
+
+    #need to figure out while loop here
+    while menuSelection != [a-zA-Z]:
+    #captures user input
+        menuSelection = input("Selection: ")
+
+    #returns data captures and stored in menuSelection
+    return(menuSelection)
 
 
-if menuSelection == "2":
-    newServerIP = input("IP Address of New Server: ")
 
-if menuSelection == "3":
-    print("Feature not implemented yet.")
-    skip = input("Press Enter to Continue...")
-    # first print list of servers denoted by number, ip, dns if applicable, ask for server number
+
+def menuSelectionLogic(menuValue):
+    print(menuValue)
+    match int(menuValue):
+        case 1:
+            print("Menu Option 1 Selected")
+        case 2:
+            print("Menu Option 2 Selected")
+        case 3:
+            print("Menu Option 3 Selected")
+        case 4:
+            return 0
+
+def main():
+    print("JDS Service Checker 1.0 Running...")
+    menuValue = menu()
+    menuSelectionLogic(menuValue)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+    exit()
+
+
+
+
+
+#  if menuSelection == "1":
+#         # Can probably break this out into it's own function after completed
+#         x = None
+#         while x is None:
+#             serverIP = input("Enter Server IP or Domain: ")
+#             print("Checking status of " + serverIP + "...")
+#             #check if the input follows an ip strucutre
+#             x = re.search(r"([\d]+\.[\d]+\.[\d]+\.[\d+]+)", serverIP)
+#                 # check if the user does NOT input a valid IP address
+#             if x == None:
+#                 print("not a valid ip")
+#             # need to re-load logic at this point
+#         # start to scan IP
+#         else:
+#             print(os.system("ping " + serverIP))
+            
+            
+
+        
+#         #check if the input follows a dns structure
+#         #if the input is neither an IP nor DNS - throw error
+#         #else we will start to check the status of the server
+
+
+#     if menuSelection == "2":
+#         newServerIP = input("IP Address of New Server: ")
+#         #validate ip address
+
+#     if menuSelection == "3":
+#         print("Feature not implemented yet.")
+#         skip = input("Press Enter to Continue...")
+#         # first print list of servers denoted by number, ip, dns if applicable, ask for server number
